355321fb48efa15b09d431de41d77a5d6d4a2b4b
allan <allan.hans68350@gmail.com>
2022-07-16 22:57:13 +0200

    affichage home dinamique + maj navbar

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,13 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\AllanRepository;
+use App\Repository\LanguageRepository;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(AllanRepository $allanRepository, LanguageRepository $languageRepository): Response
     {
+        $allan = $allanRepository->findOneBy(['prenom' => 'Allan']);
+        $languages = $languageRepository->findAll();
         return $this->render('home/index.html.twig', [
+            'allan' => $allan,
+            'languages' => $languages,
             'controller_name' => 'HomeController',
         ]);
     }
