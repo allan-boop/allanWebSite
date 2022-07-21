677e489eb54d2926054e83647bf2e8f7742a0d8d
allan <allan.hans68350@gmail.com>
2022-07-21 13:39:26 +0200

    edit allanrepository + form

---
+++ src/Form/AllanType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Allan;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class AllanType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('prenom', TextType::class)
+            ->add('nom', TextType::class)
+            ->add('age', TextType::class)
+            ->add('metier', TextType::class)
+            ->add('mail', EmailType::class)
+            ->add('telephone', TextType::class)
+            ->add('localisation', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('photo', TextType::class)
+            ->add('cv', TextType::class)
+            ->add('update', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Allan::class,
+        ]);
+    }
+}
